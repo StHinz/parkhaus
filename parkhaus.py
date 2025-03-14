ef70179d26659369cb54e39a36dd29776f92275a
StHinz <39180199+StHinz@users.noreply.github.com>
2025-03-14 07:28:42 +0100

    Create parkhaus.py

---
+++ parkhaus.py
@@ -0,0 +1,55 @@
+class Parkhaus:
+    # Konstruktor
+    def __init__(self, parkhaus_nr, ebene, plaetze_pro_ebene):
+        self.__parkhaus_nr = parkhaus_nr
+        self.__ebenen = ebene
+        self.__plaetze_pro_ebene = plaetze_pro_ebene
+        self.__parkplaetze = []
+          
+        for x in range(self.__ebenen):
+            reihe = []
+            for y in range(self.__plaetze_pro_ebene):
+                reihe.append(False)
+            self.__parkplaetze.append(reihe)
+    
+    # Getter für Parkhausnummer
+    def getParkhausNr(self):
+        return self.__parkhaus_nr
+
+    def getParkplaetze(self):
+        return self.__parkplaetze    
+
+    # Belege einen spezifischen Patz auf einer Ebene
+    def parkplatzBelegt(self,ebene,platz):
+        if self.__parkplaetze[ebene][platz] == False:
+            self.__parkplaetze[ebene][platz] = True
+            return True
+        else:
+            return False
+
+    
+    # Sperre die ganze Ebene  
+    def sperreEbene(self,ebene):
+        for i in range(len(self.__parkplaetze[ebene])):
+            self.__parkplaetze[ebene][i] = True
+
+    #Belge einen beliebigen Platz auf einer Ebene
+    def parkplatzBelegtEbene(self,ebene):
+        for i in range(len(self.__parkplaetze[ebene])):
+            if self.__parkplaetze[ebene][i] == False:
+                self.__parkplaetze[ebene][i] = True
+                return True
+                break
+        return False
+    
+    def zeigeNaechstenPlatz(self):
+        freierPlatz = []
+        for i in range(len(self.__parkplaetze)):
+            for j in range(len(self.__parkplaetze[i])):
+                if self.__parkplaetze[i][j] == False:
+                    self.__parkplaetze[i][j] = True
+                    freierPlatz.append(i)
+                    freierPlatz.append(j)
+                    break
+            break
+        return freierPlatz
